87e53046821b4b0831004399a1e1a59024bc3f0b
pthinh02 <thinh.workin@gmail.com>
2023-07-27 23:18:08 +0700

    Week 7 - Chatting socket TCP 99%

--- 03_LinuxProgramming/2_socketProgramming/chat_ServerClient/client.c
+++ 03_LinuxProgramming/2_socketProgramming/chat_ServerClient/client.c
@@ -23,37 +23,60 @@ do{	                                                    \
 #define MAX_SIZE_BUFFER 1024
 #define PORT 8080
 
-
+char empty_string[MAX_SIZE_BUFFER];
 
 int main ()
 {
+    //Declaration variables
     int ret;
     int clientfd = -1;
-    char send_msg[MAX_SIZE_BUFFER];
-    char recv_msg[MAX_SIZE_BUFFER];
+    char buffer[MAX_SIZE_BUFFER];
 
     struct sockaddr_in server_addr;
 
-    memset(send_msg, 0, sizeof(send_msg));
-    memset(recv_msg, 0, sizeof(recv_msg));
-    memset(&server_addr, 0, sizeof(server_addr));
+    memset(buffer, 0, sizeof(buffer));
 
+    memset(&server_addr, 0, sizeof(server_addr));
+    memset(empty_string, 0, sizeof(empty_string));
+
+    //Create stream socket
     clientfd = socket(AF_INET, SOCK_STREAM, 0);
     
+    //Define server address
+
     server_addr.sin_family =AF_INET;
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_addr.sin_port = htons(PORT);
 
-    //bind(clientfd, (struct sockaddr*) &server_addr, sizeof(server_addr));
 
     ret = connect(clientfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
     ERROR_CHECK(ret, "connect()");
-    do   
-    {         
-        read(clientfd, recv_msg, sizeof(recv_msg));
-        printf("[+] Server: %s\n", recv_msg);
+    while(1)  
+    {   
+        memset(buffer, 0, sizeof(buffer));
         
-    }while( !(recv_msg[0] == 'O')&&(recv_msg[1] == 'F')&&(recv_msg[2] == 'F') && (ret == 0) );
+
+        ret = read(clientfd, buffer, sizeof(buffer));
+        ERROR_CHECK(ret, "read()");
+        if(ret != -1)
+        {
+            printf("[+] Server: %s", buffer);
+            if(strncmp(buffer, "off",3) == 0)
+            {
+                break;
+            }
+        }
+        printf("[=] Client: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        
+        ret = write(clientfd, buffer, sizeof(buffer));
+        ERROR_CHECK(ret, "write()");
+
+        if(strncmp(buffer, "off", 3) == 0)
+        {
+            break;
+        }
+    };
     close(clientfd);
 
     return 0;
--- 03_LinuxProgramming/2_socketProgramming/chat_ServerClient/server.c
+++ 03_LinuxProgramming/2_socketProgramming/chat_ServerClient/server.c
@@ -11,38 +11,93 @@
 #include <sys/socket.h>
 
 #define MAX_SIZE_BUFFER 1024
-#define PORT 8080
+#define BACKLOG 50
 
-int main()
+#define ERROR_CHECK(ret, msg)                           \
+do{                                                     \
+    if(ret == -1)                                       \
+    {                                                   \
+        fprintf(stderr, "%s[%d]", __FILE__, __LINE__);  \
+        perror(msg);                                    \
+        exit(EXIT_FAILURE);                             \
+    }                                                   \
+}while(0);                                              \
+
+
+int main(int argc, char *argv[])
 {
+    //Declare
     int listenfd = -1;
     int p2pfd = -1;
+    int portno;
+    int ret;
     struct sockaddr_in server_addr, client_addr;
     int client_addr_len = sizeof(client_addr);
 
-    char send_msg[MAX_SIZE_BUFFER];
-    char recv_msg[MAX_SIZE_BUFFER];
+    char buffer[MAX_SIZE_BUFFER];
+    char buffer[MAX_SIZE_BUFFER];
     
-    memset(recv_msg, 0, sizeof(recv_msg));
-    memset(send_msg, 0, sizeof(send_msg));
+    memset(buffer, 0, sizeof(buffer));
     memset(&server_addr, 0, sizeof(server_addr));
 
+
+    if(argc < 2)
+    {
+        printf("Please provide input as <filename> <port> <ipaddress> \n");
+        exit(1);
+    }
+
+    portno = atoi(argv[1]);
+
+
+    
+
+    //Create a server socket
     listenfd = socket(AF_INET,SOCK_STREAM, 0 );
+    
+    //Define server address
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY; //inet_addr("127.0.0.1");
-    server_addr.sin_port = htons(PORT);
+    server_addr.sin_port = htons(portno);
 
-    bind(listenfd, (struct sockaddr*) &server_addr, sizeof(server_addr));
-    listen(listenfd, 50);
+
+    ret = bind(listenfd, (struct sockaddr*) &server_addr, sizeof(server_addr));
+    ERROR_CHECK(ret, "bind()");
     
+    ret = listen(listenfd, BACKLOG);
+    ERROR_CHECK(ret, "listen()");
+
+    p2pfd = accept(listenfd, (struct sockaddr*) &client_addr, &client_addr_len);
+
     while(1)
     {
-        p2pfd = accept(listenfd, (struct sockaddr*) &client_addr, &client_addr_len);
-        printf("Enter something: ");
-        fgets(send_msg, sizeof(send_msg), stdin);
-        write(p2pfd, send_msg, strlen(send_msg));
-        close(p2pfd);
-    }
+        memset(buffer, 0, sizeof(buffer));
+
+
+        printf("[+] Server: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        write(p2pfd, buffer, strlen(buffer));
+
+        if(strncmp(buffer, "off", 3) == 0 )
+        {
+            break;
+        }
+
+        ret = read(p2pfd, buffer, strlen(buffer));
+        ERROR_CHECK(ret, "read()");
+        
+        printf("%s", buffer);
+        if(strncmp(buffer, "off", 3) == 0 )
+        {
+            break;
+        }
+        if(strncmp(buffer, "0", 1) == 0)
+        {
+            continue;
+        }
+        printf("[=] Client: %s", buffer);
+    };
+    close(p2pfd);
     close(listenfd);
 
 
